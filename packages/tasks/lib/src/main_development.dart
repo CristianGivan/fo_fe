4a458ba9e8cf4d7c0b620191bb8dde765c7025ab
givanc <cristian.givan@continental-corporation.com>
2023-12-12 09:40:04 +0200

    todo was inserted

---
+++ packages/tasks/lib/src/main_development.dart
@@ -0,0 +1,13 @@
+part of tasks;
+
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final todosApi = LocalStorageTodosApi(
+    plugin: await SharedPreferences.getInstance(),
+  );
+
+  bootstrap(todosApi: todosApi);
+}
---
+++ packages/tasks/lib/tasks.dart
@@ -0,0 +1,9 @@
+library tasks;
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_todos/src/bootstrap.dart';
+import 'package:local_storage_todos_api/local_storage_todos_api.dart';
+
+part 'src/main_development.dart';
+
+
