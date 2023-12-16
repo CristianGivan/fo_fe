7f64faac8f04945e7f948c43a93ff491055e2648
givanc <cristian.givan@continental-corporation.com>
2023-12-16 17:23:56 +0200

    refactoring

---
+++ lib/features/tasks/tasks.dart
@@ -0,0 +1,17 @@
+library tasks;
+
+import 'package:flutter/material.dart';
+import 'package:fo_fe/core/db/objectbox/model.dart';
+import 'package:fo_fe/main.dart';
+import 'package:intl/intl.dart';
+
+import 'components/delete_menu.dart';
+
+part 'components/event_add.dart';
+part 'components/event_card.dart';
+part 'components/event_list_view.dart';
+part 'components/owner_select.dart';
+part 'components/task_add.dart';
+part 'components/task_card.dart';
+part 'components/task_list_view.dart';
+part 'components/task_page.dart';
