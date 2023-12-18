e8773eb8d625a2031abbc87e98a6befe4c449907
givanc <cristian.givan@continental-corporation.com>
2023-12-18 18:48:42 +0200

    working before refactoring

---
+++ lib/features/tasks/logic/tasks_bloc/tasks_state.dart
@@ -0,0 +1,14 @@
+part of 'tasks_bloc.dart';
+enum TasksBlocStatus { initial, success, failure }
+
+sealed class TasksState extends Equatable {
+  const TasksState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+final class TasksInitial extends TasksState {}
+final class TasksLoaded extends TasksState {
+
+}
