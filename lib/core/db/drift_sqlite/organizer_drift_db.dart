564b61e3fe47b0ac71b7a3c24b4773ece862cedd
givanc <cristian.givan@continental-corporation.com>
2024-06-19 10:45:27 +0300

    working

--- lib/core/db/drift_sqlite/organizer_drift_db.dart
+++ lib/core/db/drift_sqlite/organizer_drift_db.dart
@@ -1,21 +1,18 @@
-import 'dart:io';
-
 import 'package:drift/drift.dart';
-import 'package:drift/native.dart';
 import 'package:fo_fe/core/db/drift_db.dart';
 import 'package:fo_fe/features/organizer/items/task/data/drift/datasourece/task_table_drift.dart';
 import 'package:fo_fe/features/organizer/items/task/try/task_dao.dart';
-import 'package:path/path.dart' as p;
 
 import '../../../features/organizer/items/task/data/drift/datasourece/task_dao_drift.dart';
 import '../../../features/organizer/items/task/try/task_entities.dart';
+import 'connection/db_dev.dart' as db_dev;
 
 part 'organizer_drift_db.g.dart';
 
 @DriftDatabase(
     tables: [TaskTableDrift, TaskTable], daos: [TaskDaoDrift, TaskDao])
 class OrganizerDriftDB extends _$OrganizerDriftDB implements DriftDB {
-  OrganizerDriftDB() : super(_openConnection());
+  OrganizerDriftDB() : super(db_dev.connect());
 
   @override
   int get schemaVersion => 1;
@@ -27,14 +24,4 @@ class OrganizerDriftDB extends _$OrganizerDriftDB implements DriftDB {
         },
         onUpgrade: (Migrator m, int from, int to) async {},
       );
-}
-
-LazyDatabase _openConnection() {
-  return LazyDatabase(() async {
-    String projectDir = Directory.current.path;
-    final dbFolderPath = p.join(projectDir, 'lib', 'core', 'db', 'test_db');
-    await Directory(dbFolderPath).create(recursive: true);
-    final file = File(p.join(dbFolderPath, 'OrganizerDBDrift.sqlite'));
-    return NativeDatabase.createInBackground(file);
-  });
 }
