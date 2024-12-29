09585db1351f2c42aeb5a6ae3d023d9f8a1b3429
Givan, Cristian <cristian.givan@continental-corporation.com>
2024-12-29 08:12:07 +0200

    buildStateWidget

--- lib/features/organizer/all_items/task/presentation/pages/task_list_page.dart
+++ lib/features/organizer/all_items/task/presentation/pages/task_list_page.dart
@@ -7,58 +7,49 @@ class TaskListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TaskBloc, OrganizerBlocState>(
-      builder: (context, state) {
-        if (state.status == OrganizerBlocStatus.loading) {
-          return _buildLoadingState();
-        } else if (state.status == OrganizerBlocStatus.loaded) {
-          return _buildTaskListDto(context, state.displayedItems);
-        } else if (state.status == OrganizerBlocStatus.error) {
-          return _buildErrorState(state.errorMessage);
-        } else {
-          return _buildEmptyState();
-        }
-      },
-    );
+    return BlocBuilder<TaskBloc, OrganizerBlocState>(builder: (context, state) {
+      return buildStateWidget(
+        state: state,
+        buildErrorState: _buildErrorState,
+        buildLoadingState: _buildLoadingState,
+        buildLoadedState: () => _buildTaskListDto(context, state.displayedItems),
+      );
+    });
   }
-
-  Widget _buildLoadingState() => const Center(child: CircularProgressIndicator());
 
   Widget _buildErrorState(String? message) =>
       Center(child: Text(message ?? "Unknown error occurred"));
 
-  Widget _buildEmptyState() => Center(child: Text(TaskStrings().noItemsToDisplay));
+  Widget _buildLoadingState() => const Center(child: CircularProgressIndicator());
 
   Widget _buildTaskListDto(BuildContext context, OrganizerItems<ItemEntity> taskDtoList) {
     if (taskDtoList.isEmpty) {
-      return _buildEmptyState();
+      return Center(child: Text(TaskStrings().noItemsToDisplay));
     } else {
       return ListView.builder(
-        itemCount: taskDtoList.size(),
-        itemBuilder: (context, index) {
-          final taskDto = taskDtoList.getAt(index) as TaskDto;
-          return BlocBuilder<TaskUserLinkBloc, TaskUserLinkBlocState>(
-            builder: (context, state) {
-              return CheckboxListTile(
-                key: ValueKey(taskDto.id),
-                title: TaskCard(taskDto.task),
-                value: getValue(taskDto),
-                onChanged: (bool? value) {
-                  updateTaskUserLink(context, taskDto, value!);
-                },
-              );
-            },
-          );
-        },
-      );
+          itemCount: taskDtoList.size(),
+          itemBuilder: (context, index) => _buildCheckboxListTitle(taskDtoList, index, context));
     }
   }
 
-  bool getValue(TaskDto taskDto) {
+  CheckboxListTile _buildCheckboxListTitle(
+      OrganizerItems<ItemEntity> taskDtoList, int index, BuildContext context) {
+    final taskDto = taskDtoList.getAt(index) as TaskDto;
+    return CheckboxListTile(
+      key: ValueKey(taskDto.id),
+      title: TaskCard(taskDto.task),
+      value: _getValue(taskDto),
+      onChanged: (bool? value) {
+        _updateTaskUserLink(context, taskDto, value!);
+      },
+    );
+  }
+
+  bool _getValue(TaskDto taskDto) {
     return taskDto.taskUserLink.isSelectedByUser;
   }
 
-  void updateTaskUserLink(BuildContext context, TaskDto taskDto, bool value) {
+  void _updateTaskUserLink(BuildContext context, TaskDto taskDto, bool value) {
     final updatedTaskUserLink = taskDto.taskUserLink.copyWith(isSelectedByUser: value);
     context.read<TaskBloc>().add(UpdateTaskBlocEvent(TaskParams(
           taskDto: taskDto,
